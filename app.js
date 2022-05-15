21c15137aa63a42c19ee23cb38a8d4fb388df85f
Anthony Mwaura <64775294+Anthony11-hub@users.noreply.github.com>
2022-05-16 00:48:05 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function updateRoute(templateId) {//this attempts to display the templates
+    const path = window.location.pathname;
+    const route = routes[path];
+    
+    const template = document.getElementById(route.templateId);
+    const view = template.content.cloneNode(true);
+    const app = document.getElementById('app');
+    app.innerHTML = '';
+    app.appendChild(view);
+}
+
+updateRoute('login');
+
+const routes = {
+    '/login': { templateId: 'login' },
+    '/dashboard': { templateId: 'dashboard' },
+};
