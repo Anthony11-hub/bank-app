c02f4fd2529b53ae5849c670fc234220a9d5e479
Anthony Mwaura <64775294+Anthony11-hub@users.noreply.github.com>
2022-06-04 16:50:12 +0300

    fixed bug

--- app.js
+++ app.js
@@ -49,6 +49,34 @@ function onLinkClick(event){
     navigate(event.target.href);
 }
 
+async function register() {
+    const registerForm = document.getElementById('registerForm');//retrieve the data using getElementById()
+    const formData = new FormData(registerForm);//extract values from form controls as a set of key/value pairs
+    const data = Object.fromEntries(formData);//convert the data
+    const jsonData = JSON.stringify(data);//serialize the data into JSON format
+    const result = await createAccount(jsonData);
+
+    if(result.error){
+        return console.log('An error occurred:', result.error);
+    }
+
+    console.log('Account created!', result);
+}
+
+async function createAccount(account) {//async function--code that will execute asynchronously
+    try {
+      const response = await fetch('//localhost:5000/api/accounts', {//fetch api used to send JSON data to server
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: account
+      });
+      return await response.json();//to parse JSON content and return resulting object
+      //this method is asynchronous so we use await here b4 returning to make sure any errors during parsing are also caught
+    } catch (error) {
+      return { error: error.message || 'Unknown error' };
+    }
+}
+
 /**
  * makes sure that the displayed template is updated when browser history changes
  *
